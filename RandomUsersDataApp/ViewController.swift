d4a4955c9afb8b9b04772266ea4023aa45d402ef
kuznet.i.sova <kis7425910@gmail.com>
2020-06-08 23:36:29 +0300

    Initial Commit

---
+++ RandomUsersDataApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUsersDataApp
+//
+//  Created by Ирина Кузнецова on 08.06.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
