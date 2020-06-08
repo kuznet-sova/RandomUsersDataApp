8a6dfa1b096936a5194aff3f32bab1387ccf97be
kuznet.i.sova <kis7425910@gmail.com>
2020-06-09 00:27:31 +0300

    added structure application

---
+++ RandomUsersDataApp/ViewControllers/DetailInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailInfoViewController.swift
+//  RandomUsersDataApp
+//
+//  Created by Ирина Кузнецова on 09.06.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+import UIKit
+
+class DetailInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
