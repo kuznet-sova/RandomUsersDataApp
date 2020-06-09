c9d5b4d756ec770b62a2a3cf71564589108d06e8
kuznet.i.sova <kis7425910@gmail.com>
2020-06-09 21:29:26 +0300

    information passed to cells, but not unique

---
+++ RandomUsersDataApp/Models/ContactsDatabase.swift
@@ -0,0 +1,52 @@
+//
+//  ContactsDatabase.swift
+//  RandomUsersDataApp
+//
+//  Created by Ирина Кузнецова on 09.06.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+struct ContactsDatabase {
+    
+    let nameBase = ["Jack",
+                    "James",
+                    "Daniel",
+                    "Harry",
+                    "Charlie",
+                    "Ethan",
+                    "Matthew",
+                    "Ryen",
+                    "Riley",
+                    "Noah"]
+    let secondNameBase = ["Jones",
+                          "Davis",
+                          "Wilson",
+                          "Moore",
+                          "Jackson",
+                          "Harris",
+                          "Thompson",
+                          "Lee",
+                          "King",
+                          "Reed"]
+    let phoneBase = ["2(336)616-62-46",
+                     "69(599)104-72-17",
+                     "1027542874",
+                     "55(59)64322",
+                     "8(43)08682",
+                     "8(926)674-23-26",
+                     "74950100615",
+                     "7(495)414-06-25",
+                     "7(495)342-60-13",
+                     "8(926)814-13-98"]
+    let emailBase = ["ncurtis@yahoo.com",
+                     "rebeccaedwards@gmail.com",
+                     "mark32@collins-anderson.biz",
+                     "qlee@kelly-allen.com",
+                     "ujensen@page.org",
+                     "christopherharris@carr-lane.org",
+                     "wjenkins@gmail.com",
+                     "chavezcharles@gmail.com",
+                     "douglasadams@yahoo.com",
+                     "austin71@gmail.com"]
+    
+}
